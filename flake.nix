12b2bba3aaab3dd37e0e05432b7b0fb2b2a74a2c
hugosenari <hugosenari@gmail.com>
2022-04-23 18:54:14 -0300

    chore: re index github-tandy-1000-uniony

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."github-tandy-1000-uniony-master".dir   = "master";
-  inputs."github-tandy-1000-uniony-master".owner = "nim-nix-pkgs";
-  inputs."github-tandy-1000-uniony-master".ref   = "master";
-  inputs."github-tandy-1000-uniony-master".repo  = "github-tandy-1000-uniony";
-  inputs."github-tandy-1000-uniony-master".type  = "github";
-  inputs."github-tandy-1000-uniony-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."github-tandy-1000-uniony-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
